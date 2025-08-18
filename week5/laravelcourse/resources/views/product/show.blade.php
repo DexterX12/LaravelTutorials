18cbbde363d744a6ae06d3d54f47dee255125b80
DexterX12 <65998342+DexterX12@users.noreply.github.com>
2025-08-18 11:04:58 -0500

    Fix spacing in blade files

--- week5/laravelcourse/resources/views/product/show.blade.php
+++ week5/laravelcourse/resources/views/product/show.blade.php
@@ -7,23 +7,15 @@
 @section('content')
 
 <div class="card mb-3">
-
     <div class="row g-0">
-
         <div class="col-md-4">
-
             <img src="https://laravel.com/img/logotype.min.svg" class="img-fluid rounded-start">
-
         </div>
 
         <div class="col-md-8">
-
             <div class="card-body">
-
                 <h5 class="card-title">
-
                     {{ $viewData["product"]["name"] }}
-
                 </h5>
 
                 @if ($viewData["product"]["price"] > 100)
@@ -35,13 +27,9 @@
                 @foreach($viewData["product"]->comments as $comment)
                 - {{ $comment->getDescription() }}<br />
                 @endforeach
-
             </div>
-
         </div>
-
     </div>
-
 </div>
 
 @endsection
